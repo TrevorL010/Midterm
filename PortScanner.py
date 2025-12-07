824ea6d534495cc98675cb18228c99b2903218a7
Trevor Lewis <TrevorL010@yahoo.com>
2025-12-07 11:51:32 -0500

    Added "no open port" functionality and scan duration output

--- PortScanner.py
+++ PortScanner.py
@@ -4,12 +4,16 @@ from datetime import datetime
 
 def scan_ports(target_host, start_port, end_port):
     """
-    Scans a range of ports on a target host and reports open pors.
+    Scans a range of ports on a target host and reports open ports.
     """
     print("-" * 50)
     print(f"Scanning Target: {target_host}")
     print(f"Scanning started at: {datetime.now()}")
     print("-" * 50)
+
+    open_port_count = 0
+
+    start_time = datetime.now()
 
     try:
         for port in range(start_port, end_port + 1):
@@ -18,6 +22,7 @@ def scan_ports(target_host, start_port, end_port):
             result = sock.connect_ex((target_host, port))
             if result == 0:
                 print(f"Port {port}: Open")
+                open_port_count += 1
             sock.close()
     except KeyboardInterrupt:
         print("\nExiting Program !!!!")
@@ -25,3 +30,29 @@ def scan_ports(target_host, start_port, end_port):
     except socket.gaierror:
         print("\nHostname could not be resolved. Exiting")
         sys.exit()
+    except socket.error:
+        print("\nCouldn't connect to server")
+        sys.exit()
+    if open_port_count == 0:
+        print("No open ports found in the specified range.")
+
+    end_time = datetime.now()
+    duration = end_time - start_time
+    print("Scan completed in: ", duration)    
+
+
+if __name__ == "__main__":
+    target = input ("Enter the IP Address or hostname: ")
+    start_port_input = input ("Enter the starting port number: ")
+    end_port_input = input ("Enter the ending port number: ")
+
+    try:
+        start_port = int(start_port_input)
+        end_port = int(end_port_input)
+        if start_port < 0 or end_port > 65535 or start_port > end_port:
+            raise ValueError
+    except ValueError:
+        print("Please enter valid integer port numbers (0-65535).")
+        sys.exit()
+
+    scan_ports(target, start_port, end_port)        
